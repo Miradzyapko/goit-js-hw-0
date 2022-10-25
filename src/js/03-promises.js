9b0f39cb01af94c9d41e88037651ce6bd74da181
Miradzyapko <mira.dzyapko@gmail.com>
2022-10-26 01:40:38 +0300

    updfilesate

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,14 +3,7 @@ import Notiflix from 'notiflix';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
 //
 
 const delay = document.querySelector('input[name="delay"]');
